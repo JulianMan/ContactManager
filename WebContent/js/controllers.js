8bed6de3be56886e025e94fe5a6c9f3b94356409
Michal Ulman <ulman.mike@gmail.com>
2016-03-26 15:37:15 -0400

    UI without POST back to server

---
+++ WebContent/js/controllers.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/* Controllers */
+var contactManagerControllers = angular.module('contactManagerControllers', []);
+
+contactManagerControllers.controller('ContactListCtrl', function ($scope, $http) {
+	$http.get('PersonServlet/all').success(function(data){
+	//$http.get('JSON_test/contacts.json').success(function(data){
+		$scope.contacts = data;
+	});
+
+	$scope.orderProp = 'name';
+});
+
+contactManagerControllers.controller('ContactDetailCtrl', function ($scope, $routeParams, $http) {
+	
+	var id = $routeParams.personId;
+	$http.get('PersonServlet/' + id).success(function(data){
+		$scope.person = data;
+		$scope.quantity = 2;
+		$scope.leftLimit = Math.ceil(Object.keys(data.attributes).length/2);
+		$scope.rightLimit = Math.floor(Object.keys(data.attributes).length/2) + $scope.leftLimit;
+		
+		//console.log ($scope.person.attributes._photo);
+		//console.log (Object.keys(data.attributes).length);
+		//console.log ("right: " + $scope.rightLimit + ", left: " + $scope.leftLimit);
+	});
+	
+	$scope.personId = id;
+	
+});
